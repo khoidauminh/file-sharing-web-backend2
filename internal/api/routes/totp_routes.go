4dc5b68f6fd93c2c13f143b53131261a3ad2b9fa
n-hnglong <lognnh2112@gmail.com>
2025-11-19 17:56:02 +0700

    add routes for setup and verify

---
+++ internal/api/routes/totp_routes.go
@@ -0,0 +1,22 @@
+package routes
+
+import (
+	"github.com/dath-251-thuanle/file-sharing-web-backend2/internal/api/handlers"
+	"github.com/gin-gonic/gin"
+)
+
+type TotpRoutes struct {
+	handler *handlers.TotpHandler
+}
+
+func NewTotpRoutes(handler *handlers.TotpHandler) *TotpRoutes {
+	return &TotpRoutes{handler: handler}
+}
+
+func (r *TotpRoutes) Register(group *gin.RouterGroup) {
+	totp := group.Group("/auth/totp")
+	{
+		totp.POST("/setup", r.handler.SetupTOTP)
+		totp.POST("/verify", r.handler.SetupTOTP)
+	}
+}
